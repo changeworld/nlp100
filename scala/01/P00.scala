1afa6cee295b17bacb8f61d134665462790f2210
changeworld <changesworlds@gmail.com>
2016-06-06 07:01:29 +0900

    Scala 00 solved

---
+++ scala/01/P00.scala
@@ -0,0 +1,10 @@
+/**
+ * 00. 文字列の逆順
+ * 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+ */
+object P00 {
+  def main(args: Array[String]): Unit = {
+    val str = "stressed"
+    println(str.reverse)
+  }
+}
