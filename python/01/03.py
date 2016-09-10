acbf4ed47f6c02a46299b06eb53d08a3afa2b2ea
Takashi Takebayashi <changesworlds@gmail.com>
2016-09-10 11:24:41 +0900

    Python 03 solved

---
+++ python/01/03.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# 03. 円周率
+# "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+list = []
+words = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.".replace('.', "").replace(',', "").split()
+
+for word in words:
+    list.append(len(word))
+
+print list
