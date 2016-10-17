d88637b3ede40f71ffe7511f038de0d0899b8d73
Takashi Takebayashi <changesworlds@gmail.com>
2016-10-18 01:10:55 +0900

    Python 16 solved

---
+++ python/02/16.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# 16. ファイルをN分割する
+# 自然数Nをコマンドライン引数などの手段で受け取り，入力のファイルを行単位でN分割せよ．同様の処理をsplitコマンドで実現せよ．
+
+import sys, math
+
+N = int(sys.argv[1])
+
+with open(sys.argv[2]) as f:
+    lines = f.readlines()
+
+n = int(math.ceil(len(lines) / N))
+i = 0
+for i in xrange(N):
+    file = "x%s" % (i + 1)
+    out = open(file, "w")
+    for j in xrange(n):
+        try:
+            out.write(lines[i + j])
+        except:
+            break
+    i += n
+    out.close()
