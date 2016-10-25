9adeeeb19c5738094615f55843f110c824ac5b0c
Takashi Takebayashi <changesworlds@gmail.com>
2016-10-26 01:00:32 +0900

    Ruby 24 solved

---
+++ ruby/03/24.rb
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+=begin
+24. ファイル参照の抽出
+記事から参照されているメディアファイルをすべて抜き出せ．
+=end
+
+File.read("uk.txt").scan(/\[\[File:([^|\]]+)(?:\|[^\]]*)*\]\]/) do |file, _|
+  puts file
+end
