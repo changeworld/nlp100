2c3fef05511349ec7f18e0407d1c3cdb64ea5b9c
Takashi Takebayashi <changesworlds@gmail.com>
2016-10-27 01:10:52 +0900

    Ruby 25 solved

---
+++ ruby/03/25.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+=begin
+25. テンプレートの抽出
+記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ．
+=end
+
+base_info = Hash.new
+
+template_pattern = /{{基礎情報\s+国\s+(?<params>(?<markup>{{\g<markup>}} | [^{}]*)*)}}/x
+m = template_pattern.match(File.read('uk.txt'))
+
+base_info.tap do |hash|
+  m[:params].split(/^\|/).each do |entry|
+    next if entry.empty?
+    name, value = entry.split(/\s*=\s*/, 2)
+    hash[name] = value.chomp
+  end
+end
+p base_info
