6325ef043e76d3e71d4b5990b5121c7a82c2b2c2
Takashi Takebayashi <changesworlds@gmail.com>
2016-10-28 00:23:17 +0900

    Enhance

--- ruby/03/25.rb
+++ ruby/03/25.rb
@@ -5,16 +5,17 @@
 記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ．
 =end
 
-base_info = Hash.new
+def extract25
+  template_pattern = /{{基礎情報\s+国\s+(?<params>(?<markup>{{\g<markup>}} | [^{}]*)*)}}/x
+  m = template_pattern.match(File.read('uk.txt'))
 
-template_pattern = /{{基礎情報\s+国\s+(?<params>(?<markup>{{\g<markup>}} | [^{}]*)*)}}/x
-m = template_pattern.match(File.read('uk.txt'))
-
-base_info.tap do |hash|
-  m[:params].split(/^\|/).each do |entry|
-    next if entry.empty?
-    name, value = entry.split(/\s*=\s*/, 2)
-    hash[name] = value.chomp
+  Hash.new.tap do |hash|
+    m[:params].split(/^\|/).each do |entry|
+      next if entry.empty?
+      name, value = entry.split(/\s*=\s*/, 2)
+      hash[name] = value.chomp
+    end
   end
 end
-p base_info
+
+p extract25
