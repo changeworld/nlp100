70b8c5ad801d0406ee08e70cbfe1033b89107f38
Takashi Takebayashi <changesworlds@gmail.com>
2016-11-09 00:04:16 +0900

    Python 28 solved

---
+++ python/03/28.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# 28. MediaWikiマークアップの除去
+# 27の処理に加えて，テンプレートの値からMediaWikiマークアップを可能な限り除去し，国の基本情報を整形せよ．
+
+import re
+
+def remove_markup(str):
+    str = re.sub(r"'{2,5}", r"", str)
+    str = re.sub(r"\[{2}([^|\]]+?\|)*(.+?)\]{2}", r"\2", str)
+    str = re.sub(r"\{{2}.+?\|.+?\|(.+?)\}{2}", r"\1 ", str)
+    str = re.sub(r"<.*?>", r"", str)
+    str = re.sub(r"\[.*?\]", r"", str)
+    str = re.sub(r"\&pound;", r"£", str)
+    return str
+
+with open("uk.txt") as f:
+    dict = {}
+    lines = f.readline()
+    while lines:
+        line = re.search("^(.*?)\s=\s(.*)", lines)
+        if line is not None:
+            dict[line.group(1)] = remove_markup(line.group(2))
+        lines = f.readline()
+    for k, v in dict.items():
+        print "{0}, {1}".format(k, v)
