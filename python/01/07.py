4419549352a037eb52366c14877f7c616b34e8c3
Takashi Takebayashi <changesworlds@gmail.com>
2017-12-16 01:24:32 +0900

    Fix Pyflakes warnings

--- python/01/07.py
+++ python/01/07.py
@@ -3,6 +3,6 @@
 # 引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
 
 def run_template(x, y, z):
-    return unicode(x) + u'時の' + unicode(y) + u'は' + unicode(z)
+    return str(x) + '時の' + y + 'は' + str(z)
 
-print (run_template(12, u'気温', 22.4))
+print(run_template(12, '気温', 22.4))
